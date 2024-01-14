dbb843a58476fa67b7935b0e1df99fdc6fcbb9f5
visky-krisztina <127683091+visky-krisztina@users.noreply.github.com>
2024-01-14 14:28:55 +0200

    project init

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+	try {
+		mongoose.set("strictQuery", false);
+		const connect = await mongoose.connect(process.env.MONGO_URL);
+		console.log(`MongoDb Connected: ${connect.connection.host}`);
+	} catch (error) {
+		console.log(`Error: ${error.message}`);
+	}
+};
+export default connectToDatabase;
